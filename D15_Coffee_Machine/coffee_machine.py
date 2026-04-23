044f4438a4768dbd1fc7f24c57a5389a5bb80920
jorufermov <jorufermov@gmail.com>
2026-04-23 15:13:25 +0200

    Implementado report y off

--- D15_Coffee_Machine/coffee_machine.py
+++ D15_Coffee_Machine/coffee_machine.py
@@ -4,8 +4,26 @@ from data import MENU, resources
 
 #1. Prompt user by asking “​What would you like? (espresso/latte/cappuccino):​”
 
+#Declaramos variables
+money = 0
+
 #Bucle para mostrar menú
 while(True):
-    option = input("​What would you like? (espresso/latte/cappuccino):")
+    option = input("What would you like? (espresso/latte/cappuccino): ").lower().strip()
 
     #Acciones segun opcion escogida
+    match option:
+        case "espresso" | "latte" | "cappuccino":
+            #Preparar cafés
+            pass
+
+        case "report":
+            #Sacar reporte
+            print(f"Water: {resources['water']}ml")
+            print(f"Milk: {resources['milk']}ml")
+            print(f"Coffee: {resources['coffee']}g")
+            print(f"Money: ${money}")
+
+        case "off":
+            #Apagar maquina de café
+            exit()
