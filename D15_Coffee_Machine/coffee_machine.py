8883b24ee6102bbfc3746de02dd7fe330df9e5ba
jorufermov <jorufermov@gmail.com>
2026-04-23 14:35:08 +0200

    #1. Prompt user by asking “​What would you like? (espresso/latte/cappuccino):​”

---
+++ D15_Coffee_Machine/coffee_machine.py
@@ -0,0 +1,10 @@
+#Day 15. Coffee Machine Program
+
+#1. Prompt user by asking “​What would you like? (espresso/latte/cappuccino):​”
+
+#Bucle para mostrar menú
+while(True):
+    option = input("​What would you like? (espresso/latte/cappuccino):")
+
+    #Acciones segun opcion escogida
+    
