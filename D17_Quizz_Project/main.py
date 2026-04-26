3632f5584b9d41d276bfaddd97c10e57ef7da167
jorufermov <jorufermov@gmail.com>
2026-04-26 12:59:06 +0200

    Añadimos mensaje final con el score

--- D17_Quizz_Project/main.py
+++ D17_Quizz_Project/main.py
@@ -14,3 +14,7 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+    
+print("You've complete the quizz!!!")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
+print("\n")
--- D17_Quizz_Project/quiz_brain.py
+++ D17_Quizz_Project/quiz_brain.py
@@ -25,3 +25,4 @@ class QuizBrain():
             print("That's wrong.")
         print(f"The correct answer was: {correct_answer}")
         print(f"Score: {self.score}/{self.question_number}")
+        print("")
